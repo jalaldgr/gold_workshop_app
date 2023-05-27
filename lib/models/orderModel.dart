c2eb6588a56c8a7af68864c1e0b28d80c856fb3a
Jalal Yadegar <jalaldgr@gmail.com>
2023-05-27 12:12:35 +0330

    add order data model

---
+++ lib/models/orderModel.dart
@@ -0,0 +1,95 @@
+
+
+class orderData {
+  String? woocommerceOrderId;
+  String? clientFullName;
+  String? plateName;
+  String? description;
+  String? image;
+  String? code;
+  String? weight;
+  String? status;
+  // status:{type:String,enum: ['pending', 'discarded', 'inDesign','inWorkshop1','inWorkshop2','completed','completeDesign','completeWorkshop1','completeWorkshop2' ]},
+  String? workshop1fullName;
+  String? workshop1Id;
+  String? workshop1File;
+  String? workshop2fullName;
+  String? workshop2Id;
+  String? workshop2File;
+  String? designerFullName;
+  String? designerId;
+  String? designerFile;
+  String? createdDate;
+  String? id;
+
+
+
+  orderData(
+      this.woocommerceOrderId,
+      this.clientFullName,
+      this.plateName,
+      this.description,
+      this.image,
+      this.code,
+      this.weight,
+      this.status,
+      this.workshop1fullName,
+      this.workshop1Id,
+      this.workshop1File,
+      this.workshop2fullName,
+      this.workshop2Id,
+      this.workshop2File,
+      this.designerFullName,
+      this.designerId,
+      this.designerFile,
+      this.createdDate,
+      this.id
+);
+
+  orderData.fromJson(Map<String, dynamic> json) {
+    woocommerceOrderId = json['woocommerceOrderId'];
+    clientFullName = json['clientFullName'];
+    plateName = json['plateName'];
+    description = json['description'];
+    image = json['image'];
+    code = json['code'];
+    weight = json['weight'];
+    status = json['status'];
+    workshop1fullName = json['workshop1fullName'];
+    workshop1Id = json['workshop1Id'];
+    workshop1File = json['workshop1File'];
+    workshop2fullName = json['workshop2fullName'];
+    workshop2Id = json['workshop2Id'];
+    workshop2File = json['workshop2File'];
+    designerFullName = json['designerFullName'];
+    designerId = json['designerId'];
+    designerFile = json['designerFile'];
+    createdDate = json['createdDate'];
+    id = json['id'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['woocommerceOrderId'] = this.woocommerceOrderId;
+    data['clientFullName'] = this.clientFullName;
+    data['plateName'] = this.plateName;
+    data['id'] = this.id;
+    data['description']= this.description;
+    data['image']= this.image;
+    data['code']= this.code;
+    data['weight']= this.weight;
+    data['status']= this.status;
+    data['workshop1fullName']= this.workshop1fullName;
+    data['workshop1Id']= this.workshop1Id;
+    data['workshop1File']= this.workshop1File;
+    data['workshop2File']= this.workshop2File;
+    data['workshop2Id']= this.workshop2Id;
+    data['workshop2File']= this.workshop2File;
+    data['designerFullName']= this.designerFullName;
+    data['designerId']= this.designerId;
+    data['designerFile']= this.designerFile;
+    data['createdDate']= this.createdDate;
+
+    return data;
+  }
+}
