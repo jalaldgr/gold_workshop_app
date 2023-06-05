8174429a469df73d8646b97fc7da504c98eb3eff
Jalal Yadegar <jalaldgr@gmail.com>
2023-06-05 20:20:39 +0330

    add designer endpoint api

--- lib/helper/designerApi.dart
+++ lib/helper/designerApi.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
+import '../models/orderModel.dart';
 import '../models/userModel.dart';
 
 class DesignerApi {
@@ -23,4 +26,58 @@ class DesignerApi {
   }
 
 
+  static Future<List<orderData>> getPendingOrdersByDesigner() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    dynamic token = prefs.getString("jwt");
+    dynamic userID =jsonDecode(prefs.getString('user')!)['id'];
+    final response = await http.get(
+      Uri.parse('${dotenv.env['API_URL']}/designer/get-all-pending-orders/$userID/در حال طراحی/'),
+      headers: {'Authorization': 'Bearer $token'},
+    );
+    if (response.statusCode != 200) {
+      List jsonResponse = json.decode(response.body) as List;
+      return jsonResponse.map((myMap) => orderData.fromJson(myMap)).toList();
+    }
+    List jsonResponse = json.decode(response.body);
+    return jsonResponse.map((myMap) => orderData.fromJson(myMap)).toList();
+
+
+  }
+
+
+  static Future<String> completeOrderDesigner(orderData order) async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    dynamic token = prefs.getString("jwt");
+
+    final response = await http.post(
+        Uri.parse('${dotenv.env['API_URL']}/designer/complete-order/${order.id}'),
+        headers: {'Authorization': 'Bearer $token'},
+        body: order.toJson()
+    );
+    return response.body;
+  }
+
+  static Future<String> SendOrderFileDesigner(orderData order) async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    dynamic token = prefs.getString("jwt");
+    var stringResponse="no response";
+    var uri = Uri.parse('${dotenv.env['API_URL']}/designer/send-file/${order.id}/designerFile/');
+    var request2 = http.MultipartRequest('POST', uri)
+      ..headers.addAll({'Authorization': 'Bearer $token'});
+
+    if(order.designerFile!="" && order.designerFile!="null"){
+      http.MultipartFile file = await http.MultipartFile.fromPath('designerFile', "${order.designerFile}");
+      request2.files.add(file);
+    }
+
+    final response = await request2.send();
+    await response.stream.transform(utf8.decoder).listen((value) {
+      stringResponse = value;
+    });
+    print(stringResponse);
+    return stringResponse;
+
+  }
+
 }
+
