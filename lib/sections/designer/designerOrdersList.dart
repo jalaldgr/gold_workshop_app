ab4f1ec28bf67e95c5e74014540806eae0afc23b
Jalal Yadegar <jalaldgr@gmail.com>
2023-06-05 20:25:11 +0330

    add orders list and show order for designer

---
+++ lib/sections/designer/designerOrdersList.dart
@@ -0,0 +1,152 @@
+
+import 'package:flutter/material.dart';
+import 'package:gold_workshop/helper/designerApi.dart';
+import 'package:gold_workshop/models/orderModel.dart';
+import 'package:gold_workshop/sections/admin/draw_menu_admin.dart';
+import 'showDesignerOrder.dart';
+
+
+
+class DesignerOrdersList extends StatefulWidget {
+  const DesignerOrdersList({Key? key}) : super(key: key);
+
+  @override
+  _DesignerOrdersListState createState() => _DesignerOrdersListState();
+}
+
+class _DesignerOrdersListState extends State<DesignerOrdersList> {
+  TextEditingController fullNameController=TextEditingController();
+  TextEditingController userNameController=TextEditingController();
+  TextEditingController passwordController=TextEditingController();
+
+
+  @override
+  void initState() {
+
+    super.initState();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          iconTheme: IconThemeData(color: Colors.black87),
+          backgroundColor: Colors.lightGreen,
+          actions: <Widget>[IconButton(onPressed: (){setState(() {});}, icon: Icon(Icons.refresh))],
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back, color: Colors.black),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          title: const Text('لیست سفارش ها',
+              style: TextStyle(
+                  color: Colors.black87,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22.0)),
+        ),
+        drawer: SideMenuAdmin(),
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Stack(
+            children: [
+              Center(
+                child: FutureBuilder(
+                  future: DesignerApi.getPendingOrdersByDesigner(),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<List<orderData>> snapshot) =>
+                  snapshot.hasData && snapshot.data!.isNotEmpty
+                      ? Column(
+                    children: [
+
+                      ListTile(
+                        onTap: null,
+                        leading: CircleAvatar(
+                          backgroundColor: Colors.transparent,
+                        ),
+                        title:
+                        Row(mainAxisAlignment: MainAxisAlignment.start,
+                            children: <Widget>[
+                              SizedBox(height: 8,),
+                              Expanded(child: Text("ردیف" ,style: TextStyle(fontStyle: FontStyle.italic),)),
+                              Expanded(child: Text("نام مشتری",style: TextStyle(fontStyle: FontStyle.italic))),
+                              Expanded(child: Text("نوع محصول",style: TextStyle(fontStyle: FontStyle.italic))),
+                              Expanded(child: Text("تاریخ تحویل",style: TextStyle(fontStyle: FontStyle.italic))),
+                              Expanded(child: Text("وضعیت",style: TextStyle(fontStyle: FontStyle.italic))),
+                              SizedBox(width: 100,)
+                            ]
+                        ),
+                      ),
+
+                      Container(
+                        height: MediaQuery.of(context).size.height * 0.87,
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                          borderRadius:
+                          BorderRadius.only(topLeft: Radius.circular(1.0)),
+                        ),
+                        child: ListView.builder(
+                            itemCount: snapshot.data!.length,
+                            itemBuilder: (context, index) {
+                              return _buildPaymentItem(
+                                  context,
+                                  snapshot.data?[index].clientFullName,
+                                  snapshot.data?[index].status,
+                                  snapshot.data?[index],
+                                  index);
+                            }),
+                      )
+                    ],
+                  )
+                      : snapshot.hasError
+                      ? Center(
+                    child: Text('Error: ${snapshot.error}'),
+                  )
+                      : snapshot.data != null
+                      ? const Center(
+                    child: Text("بدون سفارش..."),
+                  )
+                      : CircularProgressIndicator(),
+                ),
+              )
+            ],
+          ),
+        )
+
+    );
+  }
+
+
+
+
+  Widget _buildPaymentItem(BuildContext context,
+      String? clientFullname,String? status , orderData? order,int? index) {
+    return Padding(padding: EdgeInsets.all(4),
+      child:
+      Container( color: (index! % 2 == 0) ? Colors.brown.shade50 : Colors.lightBlue.shade50,height: 100,
+        child:
+        Card(
+          child: Row(
+              children: <Widget>[
+                Expanded(child: Text("${index}")),
+                Expanded(child: Text("${order?.clientFullName}")),
+                Expanded(child: Text("${order?.productType}")),
+                Expanded(child: Text("${order?.deliveryDate}")),
+                Expanded(child: Text("${order?.status}")),
+                SizedBox(width: 100,
+                  child:Row(children: [
+                    IconButton(
+                      icon: const Icon(Icons.remove_red_eye),
+                      onPressed: () async {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ShowDesignerOrderScreen(order: order!,)));
+                      },
+                    ),
+                  ],),)
+              ]
+          ),
+        ),
+      )
+
+      ,);
+  }
+}
---
+++ lib/sections/designer/showDesignerOrder.dart
@@ -0,0 +1,209 @@
+
+
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:gold_workshop/helper/designerApi.dart';
+import 'package:gold_workshop/models/orderModel.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+
+// Define a custom Form widget.
+class ShowDesignerOrderScreen extends StatefulWidget {
+  ShowDesignerOrderScreen({super.key,required this.order});
+  orderData order;
+
+
+
+  @override
+  ShowDesignerOrderScreenState createState() {
+    return ShowDesignerOrderScreenState();
+  }
+}
+
+
+class ShowDesignerOrderScreenState extends State<ShowDesignerOrderScreen> {
+
+  List<DataRow> tableItem=[DataRow(cells: [DataCell(Text("کلید")),DataCell(Text("مقدار"))])];
+  TextEditingController imageEditTextController=TextEditingController();
+
+
+  onChangeDesignerDropDown(value){
+    setState(() {
+      widget.order.designerId = value["id"];
+      widget.order.designerFullName = value["fullName"];
+    });
+  }
+  openImagePicker() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: ['*'],
+    );
+    setState(() {
+      imageEditTextController.text = "${result?.paths[0]}";
+      widget.order.designerFile = "${result?.paths[0]}".replaceAll("\\", "\\\\");
+    });
+  }
+
+  Future<void> _launchInBrowser(Uri url) async {
+    if (!await launchUrl(
+      Uri.parse("${dotenv.env['API_URL']}/public/uploads/${widget.order.image}"),
+      mode: LaunchMode.externalApplication,
+    )) {
+      throw Exception('Could not launch $url');
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    bool isScreenWide = MediaQuery.of(context).size.width >= 720;
+    // Build a Form widget using the _formKey created above.
+    return Scaffold(
+        appBar: AppBar(
+          iconTheme: IconThemeData(color: Colors.black87),
+          backgroundColor: Colors.lightGreen,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back, color: Colors.black),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          title: const Text('مشخصات سفارش',
+              style: TextStyle(
+                  color: Colors.black87,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22.0)),
+        ),
+        body:SingleChildScrollView(
+          child: Stack(
+            children: [
+              Container(
+              decoration: BoxDecoration(color: Colors.white38),
+              child:Center(
+                child: Column(
+                  children: <Widget>[
+                    Padding(padding: EdgeInsets.all(4)
+                      ,child:
+                      Card(child:
+                      Container(padding:EdgeInsets.all(16),child:
+                      Row(
+                        children: [
+                          Expanded(child: Column(children: [Text("نام مشتری",style: TextStyle(fontSize: 14,color: Colors.grey),),Text("${widget.order.clientFullName}")],)),
+                          Expanded(child: Column(children: [Text("طراح",style: TextStyle(fontSize: 14,color: Colors.grey),),Text("${widget.order.designerFullName}")],)),
+                          Expanded(child: Column(children: [Text("نوع محصول",style: TextStyle(fontSize: 14,color: Colors.grey),),Text("${widget.order.productType}")],)),
+                          Expanded(child: Column(children: [Text("تاریخ تحویل",style: TextStyle(fontSize: 14,color: Colors.grey),),Text("${widget.order.deliveryDate}")],)),
+                        ],)
+                        ,)
+
+                        ,),
+                    ),
+                    Padding(padding: EdgeInsets.all(4),
+                      child:Card(
+                        child:
+                        Container(margin:EdgeInsets.all(16),child:
+                        // Flex(       direction: isScreenWide ? Axis.horizontal : Axis.vertical,
+                        Row(
+                          children: [
+                            Expanded(child:Column(children: [Text("توضیحات",style: TextStyle(fontSize: 14,color: Colors.grey)),Text("${widget.order.description}")])),
+                            Expanded(child: Row(children: [
+                              Expanded(child:Row(children: [Text("${widget.order.instantDelivery=="true" ? "✓ تحویل فوری":""}"),],)),
+                              Expanded(child:Row(children: [Text("${widget.order.paperDelivery=="true" ? "✓ کاغذی":""}"),],)),
+                              Expanded(child:Row(children: [Text("${widget.order.feeOrder=="true" ? "✓ بیعانه":""}"),],)),
+                              Expanded(child:Row(children: [Text("${widget.order.customerDelivery=="true" ? "✓ تحویل مشتری":""}"),],)),],
+                            )
+                            )]
+                          ,)
+                          ,),
+
+                      )
+                      ,),
+                    Padding(padding: EdgeInsets.all(4),
+                      child:Card(
+                        child: Container(margin: EdgeInsets.all(16),
+                          child:
+                          Row(
+                            children: [
+                              InkWell(onTap: (){
+                                launchUrl(Uri.parse("${dotenv.env['API_URL']}/public/uploads/${widget.order.image}"));
+
+                              },child: Expanded(child: 
+                              FadeInImage(width:256,image: NetworkImage("${dotenv.env['API_URL']}/public/uploads/${widget.order.image}"), placeholder: AssetImage("assets/images/placeholder.png"),)),),
+                              Expanded(child:
+                              Column(children: [
+                                Row(
+                                    children: []
+
+                                ),
+
+                                DataTable(
+                                  columns: const <DataColumn>[
+                                    DataColumn(
+                                      label: Text(
+                                        'ویژگی',
+                                        style: TextStyle(fontStyle: FontStyle.italic),
+                                      ),
+                                    ),
+                                    DataColumn(
+                                      label: Text(
+                                        'عنوان',
+                                        style: TextStyle(fontStyle: FontStyle.italic),
+                                      ),
+                                    ),
+
+                                  ],
+                                  rows: tableItem,
+                                )
+                              ],)
+
+
+
+                              ),
+                            ],),
+                        ),
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.all(4),
+                      child:Card(
+                        child: Container(margin: EdgeInsets.all(16),
+                          child:
+                          Row(
+                            children: [
+                              Expanded(child:TextFormField(onTap: openImagePicker,controller: imageEditTextController,decoration: InputDecoration(hintText: "انتخاب فایل"),),),
+                              Expanded(child:
+                              Column(crossAxisAlignment: CrossAxisAlignment.stretch,children: [
+                                OutlinedButton(onPressed: () async {
+                                  var res = await DesignerApi.SendOrderFileDesigner(widget.order);
+                                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("${res}")));
+                                  Navigator.pop(context);
+                                }, child: Padding(padding: EdgeInsets.all(16),child: Expanded(child: Text("ارسال فایل")  ,))
+                                ),
+                                SizedBox(height: 16,),
+                                ElevatedButton(onPressed: () async {
+                                  var res = await DesignerApi.completeOrderDesigner(widget.order);
+                                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("${res}")));
+                                  Navigator.pop(context);
+                                }, child: Padding(padding: EdgeInsets.all(16),child: Expanded(child: Text("تکمیل سفارش")  ,))
+                                ),
+                              ],)
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+
+
+                    // Add TextFormFields and ElevatedButton here.
+                  ],
+                ),
+              )
+              ,)
+              ],
+          ),
+        )
+
+
+
+
+    );
+  }
+}
