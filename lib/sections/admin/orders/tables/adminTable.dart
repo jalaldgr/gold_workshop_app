dc5232cd2bbcc4b88fcac1b13aac8feb3a7f42cc
Jalal Yadegar <jalaldgr@gmail.com>
2023-06-07 20:31:28 +0330

    add pluto grid for table 1

--- lib/sections/admin/orders/tables/adminTable.dart
+++ lib/sections/admin/orders/tables/adminTable.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:gold_workshop/helper/serverApi.dart';
 import 'package:gold_workshop/models/tableModel.dart';
 import 'package:gold_workshop/sections/admin/draw_menu_admin.dart';
+import 'package:pluto_grid/pluto_grid.dart';
 
 class AdminTableScreen extends StatefulWidget {
   const AdminTableScreen({Key? key}) : super(key: key);
@@ -19,147 +20,151 @@ class _AdminTableScreenState extends State<AdminTableScreen> {
   List<DataRow> tableItem=[];
 
 
+  List<PlutoColumn> columns = [
+
+    PlutoColumn(
+      title: 'ردیف',
+      field: 'row',
+      type: PlutoColumnType.number(),
+        enableEditingMode: true,
+      width: 100
+
+    ),
+    /// Select Column definition
+    PlutoColumn(
+        title: 'متفرقه',
+        field: 'other',
+        type: PlutoColumnType.number(),
+        enableEditingMode: true,
+        width: 100
+
+
+    ),
+    PlutoColumn(
+        title: 'بار مصرفی',
+        field: 'consumption_load',
+        type: PlutoColumnType.number(),
+        enableEditingMode: true,
+        width: 100
+
+
+    ),
+    PlutoColumn(
+        title: 'مجموع بار مصرفی و متفرقه',
+        field: 'consumptions_load_and_others',
+        type: PlutoColumnType.number(),
+        enableEditingMode: true,
+        width: 200
+
+
+    ),
+    PlutoColumn(
+        title: 'بعد از ذوب',
+        field: 'after_melt',
+        type: PlutoColumnType.number(),
+        enableEditingMode: true,
+        width: 100
+
+
+    ),
+    PlutoColumn(
+        title: 'اختلاف',
+        field: 'difference',
+        type: PlutoColumnType.number(),
+        enableEditingMode: true,
+        width: 100
+
+
+    ),
+    PlutoColumn(
+        title: 'بعد از صفحه گیری',
+        field: 'after_paging',
+        type: PlutoColumnType.number(),
+        enableEditingMode: true,
+        width: 150
+
+
+    ),
+    PlutoColumn(
+        title: 'اختلاف نهایی',
+        field: 'final_difference',
+        type: PlutoColumnType.number(),
+        enableEditingMode: true,
+        width: 120
+
+
+    ),
+
+  ];
+
+  List<PlutoRow> rows = [
+    PlutoRow(
+      cells: {
+        'row': PlutoCell(value: 2020),
+        'other': PlutoCell(value: 2020),
+        'consumption_load': PlutoCell(value: 2020),
+        'consumptions_load_and_others': PlutoCell(value: 2020),
+        'after_melt': PlutoCell(value: 2020),
+        'difference': PlutoCell(value: 2020),
+        'after_paging': PlutoCell(value: 2020),
+        'final_difference': PlutoCell(value: 2020),
+
+      },
+    ),
+  ];
+
+  /// columnGroups that can group columns can be omitted.
+
+
   @override
   void initState() {
 
     super.initState();
   }
 
+  late final PlutoGridStateManager stateManager;
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          iconTheme: IconThemeData(color: Colors.black87),
-          backgroundColor: Colors.pink,
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back, color: Colors.black),
-            onPressed: () => Navigator.of(context).pop(),
-          ),
-          actions: <Widget>[IconButton(onPressed: (){setState(() {});}, icon: Icon(Icons.refresh))],
-          title: const Text('جدول های امروز',
-              style: TextStyle(
-                  color: Colors.black87,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 22.0)),
-        ),
-        drawer: SideMenuAdmin(),
-        backgroundColor: Colors.white,
-        body:SingleChildScrollView(child:
-          Stack(
-            children: [
-              FutureBuilder(
-                future: AdminApi.getTable(),
-                builder: (BuildContext context,
-                    AsyncSnapshot<tableData> snapshot) =>
-                snapshot.hasData
-                    ?
-                Padding(padding: EdgeInsets.all(4),
-                    child:
-                    Column(
-                      children: [
-                        Card(
-                          child: Container(alignment: Alignment.center,padding: EdgeInsets.all(8),
-                            child: Column(children: [
-                              DataTable(columns: const <DataColumn>[
-                                        DataColumn(
-                                          label: Text(
-                                            'ردیف',
-                                            style: TextStyle(
-                                                fontStyle: FontStyle.italic),
-                                          ),
-                                        ),
-                                        DataColumn(
-                                          label: Text(
-                                            'متفرقه',
-                                            style: TextStyle(
-                                                fontStyle: FontStyle.italic),
-                                          ),
-                                        ),
-                                DataColumn(
-                                  label: Text(
-                                    'بار مصرفی',
-                                    style: TextStyle(
-                                        fontStyle: FontStyle.italic),
-                                  ),
-                                ),
-                                DataColumn(
-                                  label: Text(
-                                    'مجموع متفرقه و بار',
-                                    style: TextStyle(
-                                        fontStyle: FontStyle.italic),
-                                  ),
-                                ),
-                                DataColumn(
-                                  label: Text(
-                                    'بعد از ذوب',
-                                    style: TextStyle(
-                                        fontStyle: FontStyle.italic),
-                                  ),
-                                ),
-                                DataColumn(
-                                  label: Text(
-                                    'اختلاف',
-                                    style: TextStyle(
-                                        fontStyle: FontStyle.italic),
-                                  ),
-                                ),
-                                DataColumn(
-                                  label: Text(
-                                    'بعد از صفحه گیری',
-                                    style: TextStyle(
-                                        fontStyle: FontStyle.italic),
-                                  ),
-                                ),
-                                DataColumn(
-                                  label: Text(
-                                    'اختلاف نهایی',
-                                    style: TextStyle(
-                                        fontStyle: FontStyle.italic),
-                                  ),
-                                ),
+      body: Container(
+        padding: const EdgeInsets.all(15),
+        child: PlutoGrid(
+          columns: columns,
+          rows: rows,
 
-                                      ], rows: tableItem
-                              )
-                            ]),
-                          ),
-                        ),
-                      ],
-                    )
-                )
-                    : snapshot.hasError
-                    ? Center(
-                      child: Text('Error: ${snapshot.error}'),)
-                    : snapshot.data != null
-                    ? const Center(
-                      child: Text("..."),)
-                    : CircularProgressIndicator(),
-              )
-            ],),)
+          onLoaded: (PlutoGridOnLoadedEvent event) {
+            stateManager = event.stateManager;
 
-        ,
-        floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.add),
-          onPressed: () {
-            int rowNumber = tableItem.length+1;
-            setState(() {
-              tableItem.add(DataRow(cells: [
-                DataCell(Text("${rowNumber}")) ,
-                DataCell(Text("0")),
-                DataCell(Text("0")),
-                DataCell(Text("0")),
-                DataCell(Text("0")),
-                DataCell(Text("0")),
-                DataCell(Text("0")),
-                DataCell(Text("0"))
-              ]));
-
-            });
+            // stateManager.setShowColumnFilter(true);
           },
-        )
+          onChanged: (PlutoGridOnChangedEvent event) {
+            print(event);
+          },
+          configuration: const PlutoGridConfiguration(style: PlutoGridStyleConfig()),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        stateManager.insertRows(0, [
+          PlutoRow(
+            cells: {
+              'row': PlutoCell(value: 2020),
+              'other': PlutoCell(value: 2020),
+              'consumption_load': PlutoCell(value: 2020),
+              'consumptions_load_and_others': PlutoCell(value: 2020),
+              'after_melt': PlutoCell(value: 2020),
+              'difference': PlutoCell(value: 2020),
+              'after_paging': PlutoCell(value: 2020),
+              'final_difference': PlutoCell(value: 2020),
+
+            },
+          ),
+        ]);
+        print("gihiii");
+        },child: const Icon(Icons.add)),
 
     );
   }
-
-
 
 
 }
