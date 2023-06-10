be9b410af8472c568692c1453bd12cb73d4f8c26
Jalal Yadegar <jalaldgr@gmail.com>
2023-06-10 09:12:11 +0330

    update table 5

--- lib/sections/tables/Table5.dart
+++ lib/sections/tables/Table5.dart
@@ -22,26 +22,170 @@ class _Table5ScreenState extends State<Table5Screen> {
 
 
   List<PlutoColumn> columns = [
-
-    PlutoColumn (
-      title: 'ردیف',
-      field: 'row',
-      type: PlutoColumnType.number(),
+    PlutoColumn(
+        title: 'ردیف',
+        field: 'row',
+        type: PlutoColumnType.number(),
         enableEditingMode: true,
-      width: 100
+        width: 100),
 
-    ),
     /// Select Column definition
     PlutoColumn(
         title: 'سفارش دهنده',
         field: 'client_name',
         type: PlutoColumnType.text(),
         enableEditingMode: true,
-        width: 100
-
-
-    ),
-
+        width: 100),
+    PlutoColumn(
+        title: 'شرح',
+        field: 'description',
+        type: PlutoColumnType.text(),
+        enableEditingMode: true,
+        width: 100),
+    PlutoColumn(
+        title: 'کد',
+        field: 'code',
+        type: PlutoColumnType.text(),
+        enableEditingMode: true,
+        width: 100),
+    PlutoColumn(
+        title: 'وزن',
+        field: 'weight',
+        type: PlutoColumnType.number(),
+        enableEditingMode: true,
+        width: 100),
+    PlutoColumn(
+        title: 'کسر برش',
+        field: 'cut_deficiency',
+        type: PlutoColumnType.number(),
+        enableEditingMode: true,
+        width: 100),
+    PlutoColumn(
+        title: 'کسر پاپیون',
+        field: 'popion_deficiency',
+        type: PlutoColumnType.number(),
+        enableEditingMode: true,
+        width: 100),
+    PlutoColumn(
+        title: 'وزن نگین',
+        field: 'jewel_weight',
+        type: PlutoColumnType.number(),
+        enableEditingMode: true,
+        width: 100),
+    PlutoColumn(
+        title: 'توضیحات',
+        field: 'summary',
+        type: PlutoColumnType.number(),
+        enableEditingMode: true,
+        width: 100),
+    PlutoColumn(
+        title: 'ذوب',
+        field: 'melting',
+        type: PlutoColumnType.number(),
+        enableEditingMode: true,
+        width: 100),
+    PlutoColumn(
+        title: 'مفتول کشی',
+        field: 'wire_pulling',
+        type: PlutoColumnType.number(),
+        enableEditingMode: true,
+        width: 100),
+    PlutoColumn(
+        title: 'برش',
+        field: 'cut',
+        type: PlutoColumnType.number(),
+        enableEditingMode: true,
+        width: 100),
+    PlutoColumn(
+        title: 'پاپیون',
+        field: 'popion',
+        type: PlutoColumnType.number(),
+        enableEditingMode: true,
+        width: 100),
+    PlutoColumn(
+        title: 'حلقه',
+        field: 'ring',
+        type: PlutoColumnType.number(),
+        enableEditingMode: true,
+        width: 100),
+    PlutoColumn(
+        title: 'زنجیر',
+        field: 'chain',
+        type: PlutoColumnType.number(),
+        enableEditingMode: true,
+        width: 100),
+    PlutoColumn(
+        title: 'تکه زنجیر',
+        field: 'piece_chain',
+        type: PlutoColumnType.number(),
+        enableEditingMode: true,
+        width: 100),
+    PlutoColumn(
+        title: 'عصایی',
+        field: 'cane',
+        type: PlutoColumnType.number(),
+        enableEditingMode: true,
+        width: 100),
+    PlutoColumn(
+        title: 'مدبر',
+        field: 'lock',
+        type: PlutoColumnType.number(),
+        enableEditingMode: true,
+        width: 100),
+    PlutoColumn(
+        title: 'مفتول',
+        field: 'wire',
+        type: PlutoColumnType.number(),
+        enableEditingMode: true,
+        width: 100),
+    PlutoColumn(
+        title: 'لحیم',
+        field: 'solder',
+        type: PlutoColumnType.number(),
+        enableEditingMode: true,
+        width: 100),
+    PlutoColumn(
+        title: 'گوی',
+        field: 'ball',
+        type: PlutoColumnType.number(),
+        enableEditingMode: true,
+        width: 100),
+    PlutoColumn(
+        title: 'سرسنجاق',
+        field: 'pin',
+        type: PlutoColumnType.number(),
+        enableEditingMode: true,
+        width: 100),
+    PlutoColumn(
+        title: 'رینگ',
+        field: 'ring2',
+        type: PlutoColumnType.number(),
+        enableEditingMode: true,
+        width: 100),
+    PlutoColumn(
+        title: 'نیم ساخته',
+        field: 'half_made',
+        type: PlutoColumnType.number(),
+        enableEditingMode: true,
+        width: 100),
+    PlutoColumn(
+        title: 'مفتول کشی',
+        field: 'wiring',
+        type: PlutoColumnType.number(),
+        enableEditingMode: true,
+        width: 100),
+    PlutoColumn(
+        title: 'وزن نهایی',
+        field: 'final_weight',
+        type: PlutoColumnType.number(),
+        enableEditingMode: true,
+        width: 100),
+    PlutoColumn(
+        title: 'کسر پرداخت',
+        field: 'burnish_deficiency',
+        type: PlutoColumnType.number(),
+        enableEditingMode: true,
+        width: 100),
   ];
 
   List<PlutoRow> rows = [
@@ -49,8 +193,31 @@ class _Table5ScreenState extends State<Table5Screen> {
       cells: {
         'row': PlutoCell(value: 1),
         'client_name': PlutoCell(value: ""),
-
-
+        'description':PlutoCell(value: ""),
+        'code':PlutoCell(value: ""),
+        'weight':PlutoCell(value: 0),
+        'cut_deficiency':PlutoCell(value: 0),
+        'popion_deficiency':PlutoCell(value: 0),
+        'jewel_weight':PlutoCell(value: 0),
+        'summary':PlutoCell(value: ""),
+        'melting':PlutoCell(value: 0),
+        'wire_pulling':PlutoCell(value: 0),
+        'cut':PlutoCell(value: 0),
+        'popion':PlutoCell(value: 0),
+        'ring':PlutoCell(value:0),
+        'chain':PlutoCell(value: 0),
+        'piece_chain':PlutoCell(value: 0),
+        'cane':PlutoCell(value: 0),
+        'lock':PlutoCell(value: 0),
+        'wire':PlutoCell(value: 0),
+        'solder':PlutoCell(value: 0),
+        'ball':PlutoCell(value: 0),
+        'pin':PlutoCell(value: 0),
+        'ring2':PlutoCell(value: 0),
+        'half_made':PlutoCell(value: 0),
+        'wiring':PlutoCell(value: 0),
+        'final_weight':PlutoCell(value: 0),
+        'burnish_deficiency':PlutoCell(value: 0),
       },
     ),
   ];
@@ -65,6 +232,33 @@ class _Table5ScreenState extends State<Table5Screen> {
       dynamic table5 =json.decode(tables.table5!);
       List rowNumber = table5["row"];
       List client_name = table5["client_name"];
+      List description = table5["description"];
+      List code = table5["code"];
+      List weight = table5["weight"];
+      List cut_deficiency = table5["cut_deficiency"];
+      List popion_deficiency = table5["popion_deficiency"];
+      List jewel_weight = table5["jewel_weight"];
+      List summary = table5["summary"];
+      List wire_pulling = table5["wire_pulling"];
+      List melting = table5["melting"];
+      List cut = table5["cut"];
+      List popion = table5["popion"];
+      List ring = table5["ring"];
+      List chain = table5["chain"];
+      List piece_chain = table5["piece_chain"];
+      List cane = table5["cane"];
+      List lock = table5["lock"];
+      List wire = table5["wire"];
+      List solder = table5["solder"];
+      List ball = table5["ball"];
+      List pin = table5["pin"];
+      List ring2 = table5["ring2"];
+      List half_made = table5["half_made"];
+      List wiring = table5["wiring"];
+      List final_weight = table5["final_weight"];
+      List burnish_deficiency = table5["burnish_deficiency"];
+
+
 
       List<PlutoRow> updatedRows=[];
       for (var i = 0; i < rowNumber.length; i++) {
@@ -72,18 +266,38 @@ class _Table5ScreenState extends State<Table5Screen> {
           cells: {
             'row': PlutoCell(value:rowNumber[i] ),
             'client_name': PlutoCell(value: client_name[i]),
+            'description': PlutoCell(value: description[i]),
+            'code': PlutoCell(value: code[i]),
+            'weight': PlutoCell(value: weight[i]),
+            'cut_deficiency': PlutoCell(value: cut_deficiency[i]),
+            'popion_deficiency': PlutoCell(value: popion_deficiency[i]),
+            'jewel_weight': PlutoCell(value: jewel_weight[i]),
+            'summary': PlutoCell(value: summary[i]),
+            'melting': PlutoCell(value: melting[i]),
+            'wire_pulling': PlutoCell(value: wire_pulling[i]),
+            'cut': PlutoCell(value: cut[i]),
+            'popion': PlutoCell(value: popion[i]),
+            'ring': PlutoCell(value: ring[i]),
+            'chain': PlutoCell(value: chain[i]),
+            'piece_chain': PlutoCell(value: piece_chain[i]),
+            'cane': PlutoCell(value: cane[i]),
+            'lock': PlutoCell(value: lock[i]),
+            'wire': PlutoCell(value: wire[i]),
+            'solder': PlutoCell(value: solder[i]),
+            'ball': PlutoCell(value: ball[i]),
+            'pin': PlutoCell(value: pin[i]),
+            'ring2': PlutoCell(value: ring2[i]),
+            'half_made': PlutoCell(value: half_made[i]),
+            'wiring': PlutoCell(value: wiring[i]),
+            'final_weight': PlutoCell(value: final_weight[i]),
+            'burnish_deficiency': PlutoCell(value: burnish_deficiency[i]),
           },
         ));
       }
-
       stateManager.refRows.clear();
       stateManager.insertRows(0, updatedRows);
       stateManager.setShowLoading(false);
-
-
     }
-
-
   }
 
   @override
@@ -135,7 +349,32 @@ class _Table5ScreenState extends State<Table5Screen> {
           PlutoRow(
             cells: {
               'row': PlutoCell(value: 1+stateManager.rows.length),
-              'client_name': PlutoCell(value: 0),
+              'client_name': PlutoCell(value: ""),
+              'description': PlutoCell(value: ""),
+              'code': PlutoCell(value: ""),
+              'weight': PlutoCell(value: 0),
+              'cut_deficiency': PlutoCell(value: 0),
+              'popion_deficiency': PlutoCell(value: 0),
+              'jewel_weight': PlutoCell(value: 0),
+              'summary': PlutoCell(value: ""),
+              'melting': PlutoCell(value: 0),
+              'wire_pulling': PlutoCell(value: 0),
+              'cut': PlutoCell(value: 0),
+              'popion': PlutoCell(value: 0),
+              'ring': PlutoCell(value: 0),
+              'chain': PlutoCell(value: 0),
+              'piece_chain': PlutoCell(value: 0),
+              'cane': PlutoCell(value: 0),
+              'lock': PlutoCell(value: 0),
+              'wire': PlutoCell(value: 0),
+              'solder': PlutoCell(value: 0),
+              'ball': PlutoCell(value: 0),
+              'pin': PlutoCell(value: 0),
+              'ring2': PlutoCell(value: 0),
+              'half_made': PlutoCell(value: 0),
+              'wiring': PlutoCell(value: 0),
+              'final_weight': PlutoCell(value: 0),
+              'burnish_deficiency': PlutoCell(value: 0),
             },
           ),
         ]);
@@ -143,10 +382,63 @@ class _Table5ScreenState extends State<Table5Screen> {
 
         List rows = stateManager.rows.map((e) => e.cells['row']?.value).toList();
         List client_name = stateManager.rows.map((e) => e.cells['client_name']?.value).toList();
+        List description = stateManager.rows.map((e) => e.cells['description']?.value).toList();
+        List code = stateManager.rows.map((e) => e.cells['code']?.value).toList();
+        List weight = stateManager.rows.map((e) => e.cells['weight']?.value).toList();
+        List cut_deficiency = stateManager.rows.map((e) => e.cells['cut_deficiency']?.value).toList();
+        List popion_deficiency = stateManager.rows.map((e) => e.cells['popion_deficiency']?.value).toList();
+        List jewel_weight = stateManager.rows.map((e) => e.cells['jewel_weight']?.value).toList();
+        List summary = stateManager.rows.map((e) => e.cells['summary']?.value).toList();
+        List melting = stateManager.rows.map((e) => e.cells['melting']?.value).toList();
+        List wire_pulling = stateManager.rows.map((e) => e.cells['wire_pulling']?.value).toList();
+        List cut = stateManager.rows.map((e) => e.cells['cut']?.value).toList();
+        List popion = stateManager.rows.map((e) => e.cells['popion']?.value).toList();
+        List ring = stateManager.rows.map((e) => e.cells['ring']?.value).toList();
+        List chain = stateManager.rows.map((e) => e.cells['chain']?.value).toList();
+        List piece_chain = stateManager.rows.map((e) => e.cells['piece_chain']?.value).toList();
+        List cane = stateManager.rows.map((e) => e.cells['cane']?.value).toList();
+        List lock = stateManager.rows.map((e) => e.cells['lock']?.value).toList();
+        List wire = stateManager.rows.map((e) => e.cells['wire']?.value).toList();
+        List solder = stateManager.rows.map((e) => e.cells['solder']?.value).toList();
+        List ball = stateManager.rows.map((e) => e.cells['ball']?.value).toList();
+        List pin = stateManager.rows.map((e) => e.cells['pin']?.value).toList();
+        List ring2 = stateManager.rows.map((e) => e.cells['ring2']?.value).toList();
+        List half_made = stateManager.rows.map((e) => e.cells['half_made']?.value).toList();
+        List wiring = stateManager.rows.map((e) => e.cells['wiring']?.value).toList();
+        List final_weight = stateManager.rows.map((e) => e.cells['final_weight']?.value).toList();
+        List burnish_deficiency = stateManager.rows.map((e) => e.cells['burnish_deficiency']?.value).toList();
+
+
 
 
         table5["row"]=rows.toList();
         table5["client_name"]=client_name.toList();
+        table5["description"]=description.toList();
+        table5["code"]=code.toList();
+        table5["weight"]=weight.toList();
+        table5["cut_deficiency"]=cut_deficiency.toList();
+        table5["popion_deficiency"]=popion_deficiency.toList();
+        table5["jewel_weight"]=jewel_weight.toList();
+        table5["summary"]=summary.toList();
+        table5["melting"]=melting.toList();
+        table5["wire_pulling"]=wire_pulling.toList();
+        table5["cut"]=cut.toList();
+        table5["popion"]=popion.toList();
+        table5["ring"]=ring.toList();
+        table5["chain"]=chain.toList();
+        table5["piece_chain"]=piece_chain.toList();
+        table5["cane"]=cane.toList();
+        table5["lock"]=lock.toList();
+        table5["wire"]=wire.toList();
+        table5["solder"]=solder.toList();
+        table5["ball"]=ball.toList();
+        table5["pin"]=pin.toList();
+        table5["ring2"]=ring2.toList();
+        table5["half_made"]=half_made.toList();
+        table5["wiring"]=wiring.toList();
+        table5["final_weight"]=final_weight.toList();
+        table5["burnish_deficiency"]=burnish_deficiency.toList();
+
 
 
         tableData t = new tableData("تکمیل کارگاه 1", "", "", "","", "", jsonEncode(table5), "");
