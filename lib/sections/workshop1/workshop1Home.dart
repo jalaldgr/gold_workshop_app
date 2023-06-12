85cbd654f01a8b7c9c3bb38b81776f6bef2134c2
Jalal Yadegar <jalaldgr@gmail.com>
2023-06-12 17:00:11 +0330

    update workshop1 home screen

--- lib/sections/workshop1/workshop1Home.dart
+++ lib/sections/workshop1/workshop1Home.dart
@@ -8,6 +8,9 @@ import 'package:gold_workshop/sections/workshop1/draw_menu_workshop1.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
+import '../../helper/serverApi.dart';
+import '../tables/Table1.dart';
+import '../tables/Table2.dart';
 import 'workshop1OrdersList.dart';
 
 
@@ -28,6 +31,7 @@ class Workshop1HomeScreen extends StatefulWidget {
 }
 
 class _Workshop1HomeScreenState extends State<Workshop1HomeScreen> {
+  dynamic table6;
 
 
   @override
@@ -57,6 +61,87 @@ class _Workshop1HomeScreenState extends State<Workshop1HomeScreen> {
                   Expanded(
                       child: Padding(
                         padding: EdgeInsets.all(32),
+                        child:Column(crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              Expanded(child:
+                              Row(crossAxisAlignment: CrossAxisAlignment.stretch,
+                                children: [
+                                Expanded(child:Padding(padding: EdgeInsets.all(4),child:
+                                OutlinedButton(
+                                    onPressed: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) => Table1Screen(headerColor: Colors.amber,)));
+                                    },
+                                    child: Text("جدول 1",style: TextStyle(fontSize: 32),))
+                                  ,)),
+                                Expanded(child:
+                                Padding(padding: EdgeInsets.all(4),
+                                  child: OutlinedButton(
+                                      onPressed: () {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) => Table2Screen(headerColor: Colors.amber)));
+                                      },
+                                      child: Text("جدول 2",style: TextStyle(fontSize: 32),))
+                                  ,)
+
+                                ),
+                              ],)),
+                              Expanded(child: Column(children: [
+                                Expanded(child:
+                                Card(color: Colors.blue.shade100,
+                                  child: Container(
+                                    padding: EdgeInsets.all(8),
+                                    child: Column(
+                                      children: [
+                                        Container(
+                                          child:
+                                          Row(crossAxisAlignment: CrossAxisAlignment.center,
+                                            children: [
+                                              Expanded(child: Text("ذوب روزانه",style: TextStyle(fontWeight: FontWeight.w100),textAlign: TextAlign.center)),
+                                              Expanded(child: Text("کسر پرداخت",style: TextStyle(fontWeight: FontWeight.w100),textAlign: TextAlign.center)),
+                                              Expanded(child: Text("کسر ذوب",style: TextStyle(fontWeight: FontWeight.w100),textAlign: TextAlign.center)),
+                                              Expanded(child: Text("کسر برش",style: TextStyle(fontWeight: FontWeight.w100),textAlign: TextAlign.center)),
+                                              Expanded(child: Text("اختلاف برش",style: TextStyle(fontWeight: FontWeight.w100),textAlign: TextAlign.center)),
+                                              Expanded(child: Text("مجموع",style: TextStyle(fontWeight: FontWeight.w100),textAlign: TextAlign.center)),
+                                            ],
+                                          )
+                                          ,)
+                                        ,
+                                        SizedBox(height: 4,),
+                                        InkWell(child:
+                                        Container(
+                                          decoration:
+                                          BoxDecoration(color: Colors.blue.shade100),
+                                          child: FutureBuilder(
+                                            future: fetchTables(),
+                                            builder: (BuildContext context,
+                                                AsyncSnapshot<dynamic> snapshot) =>
+                                            snapshot.hasData
+                                                ? _buildTableItem(context, snapshot.data)
+                                                : CircularProgressIndicator(),
+                                          ),
+                                        )
+                                          ,
+                                          onTap: (){setState(() {
+
+                                          });},
+                                        )
+
+                                      ],
+                                    ),
+                                  ),
+                                ))
+                              ],))
+                            ],
+                        ),
+                      )),
+                  Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.all(32),
                         child: OutlinedButton(
                             onPressed: () {
                               Navigator.push(
@@ -65,18 +150,6 @@ class _Workshop1HomeScreenState extends State<Workshop1HomeScreen> {
                                       builder: (context) => Workshop1OrdersList()));
                             },
                             child: Text("سفارش ها",style: TextStyle(fontSize: 32),)),
-                      )),
-                  Expanded(
-                      child: Padding(
-                        padding: EdgeInsets.all(32),
-                        child: OutlinedButton(
-                            onPressed: () {
-                              // Navigator.push(
-                              //     context,
-                              //     MaterialPageRoute(
-                              //         builder: (context) => Workshop1OrdersList()));
-                            },
-                            child: Text("جدول ها",style: TextStyle(fontSize: 32),)),
                       ))        ])));
   }
 
@@ -84,6 +157,54 @@ class _Workshop1HomeScreenState extends State<Workshop1HomeScreen> {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.remove('jwt');
     Navigator.of(context).maybePop();
+  }
+  Future fetchTables() async {
+    var tables = await AdminApi.getTable();
+    table6 =json.decode(tables.table6!);
+    if(tables.table6!.length>10){
+      return  table6 =json.decode(tables.table6!);
+    }
+
+
+  }
+  Widget _buildTableItem(BuildContext context, dynamic table) {
+    return Row(
+      children: [
+        Expanded(
+            child: Text("${table["daily_melt"]!=null? table["daily_melt"].toStringAsFixed(3): "0"}",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center)),
+        Expanded(
+            child: Text("${table["burnish_deficiency"].toStringAsFixed(3)?? "0"}",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center)),
+        Expanded(
+            child: Text("${table["melt_deficiency"].toStringAsFixed(3)?? "0"}",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center)),
+        Expanded(
+            child: Text("${table["cut_deficiency"].toStringAsFixed(3)?? "0"}",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center)),
+        Expanded(
+            child: Text("${table["cut_deference"].toStringAsFixed(3)?? "0"}",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center)),
+        Expanded(
+            child: Text(
+              "${table["sum"].toStringAsFixed(3)?? "0"}",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            )),
+      ],
+    );
   }
 
 
