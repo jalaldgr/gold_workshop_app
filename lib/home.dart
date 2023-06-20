1a65002b80fa66a2961dbb62ec6e908128405bc1
Jalal Yadegar <jalaldgr@gmail.com>
2023-06-20 19:53:08 +0330

    remove save logins, add splash screen

---
+++ lib/home.dart
@@ -0,0 +1,118 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:gold_workshop/sections/workshop1/workshop1Home.dart';
+import 'package:gold_workshop/sections/workshop2/workshop2Home.dart';
+import 'package:http/http.dart' as http;
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+import 'login.dart';
+import 'sections/admin/adminHome.dart';
+import 'sections/designer/designerHome.dart';
+
+
+class HomeScreen extends StatefulWidget {
+  @override
+  LoginScreen createState() => LoginScreen();
+}
+
+class LoginScreen extends State<HomeScreen> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    Timer(Duration(seconds: 2),
+            ()=>    navigate());
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child:Column(children: [
+
+            Image.asset("assets/images/logo.png",width: 768,),
+            SizedBox(height: 32),
+          ],)
+        ),
+      ),
+    );
+  }
+
+  navigate() async {
+    //disable save logins
+
+    await dotenv.load(fileName: "assets/env");
+    // SharedPreferences prefs = await SharedPreferences.getInstance();
+    // final info = await PackageInfo.fromPlatform();
+    // prefs.setString("version",info.version );
+    // var jwtToken = prefs.getString("jwt");
+
+    // dynamic role =jsonDecode(prefs.getString('user')!)['role']; // get driver user id
+    //
+    // if (jwtToken != null) {
+    //   dynamic str = jwtToken;
+    //   dynamic jwt = str.length > 1 ? str.toString().split(".") : "";
+    //   if (jwt.length != 3) {
+    //     Navigator.of(context).pushAndRemoveUntil(
+    //         MaterialPageRoute(
+    //             builder: (BuildContext context) =>
+    //                 LoginPage()),
+    //             (Route<dynamic> route) => false);
+    //   } else {
+    //     var payload = json.decode(
+    //         ascii.decode(base64.decode(base64.normalize(jwt[1]))));
+    //
+    //     if (DateTime.fromMillisecondsSinceEpoch(payload["exp"] * 1000).isAfter(DateTime.now())) {
+    //
+    //               switch(role){
+    //                 case "Admin":
+    //                   Navigator.of(context).push(
+    //                       MaterialPageRoute(
+    //                           builder: (BuildContext context) =>
+    //                               AdminHomeScreen(str, payload)),
+    //                           );
+    //                   break;
+    //                 case "Designer":
+    //                   Navigator.of(context).pushAndRemoveUntil(
+    //                       MaterialPageRoute(
+    //                           builder: (BuildContext context) =>
+    //                               DesignerHomeScreen(str, payload)),
+    //                           (Route<dynamic> route) => false);
+    //                   break;
+    //                 case "Workshop1":
+    //                   Navigator.of(context).pushAndRemoveUntil(
+    //                       MaterialPageRoute(
+    //                           builder: (BuildContext context) =>
+    //                               Workshop1HomeScreen(str, payload)),
+    //                           (Route<dynamic> route) => false);
+    //                   break;
+    //                 case "Workshop2":
+    //                   Navigator.of(context).pushAndRemoveUntil(
+    //                       MaterialPageRoute(
+    //                           builder: (BuildContext context) =>
+    //                               Workshop1HomeScreen(str, payload)),
+    //                           (Route<dynamic> route) => false);
+    //                   break;
+    //
+    //               }
+    //     }
+    //   }
+    // } else {
+      Navigator.of(context).pushAndRemoveUntil(
+          MaterialPageRoute(
+              builder: (BuildContext context) =>
+                  LoginPage()),
+              (Route<dynamic> route) => false);
+    // }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,18 @@
 
 
-import 'dart:convert' show json, base64, ascii;
 import 'dart:ui';
 import "dart:convert";
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:gold_workshop/sections/workshop1/workshop1Home.dart';
-import 'package:gold_workshop/sections/workshop2/workshop2Home.dart';
+import 'package:gold_workshop/Home.dart';
+
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:persian_fonts/persian_fonts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:url_strategy/url_strategy.dart';
 
-import 'sections/admin/adminHome.dart';
-import 'login.dart';
-import 'sections/designer/designerHome.dart';
+
 
 Future<void> main() async {
   runApp(MyApp());
@@ -25,22 +21,7 @@ Future<void> main() async {
 }
 
 class MyApp extends StatelessWidget {
-  Future<String> get jwtOrEmpty async {
-    await dotenv.load(fileName: "assets/env");
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    final info = await PackageInfo.fromPlatform();
-    prefs.setString("version",info.version );
-    var jwt = prefs.getString("jwt");
-    if (jwt == null) return "N";
-    return jwt.toString();
-  }
 
-  Future<String> get userRoleType async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    dynamic role =jsonDecode(prefs.getString('user')!)['role']; // get driver user id
-    if (role == null) return "N";
-    return role;
-  }
 
   // This widget is the root of your application.
   @override
@@ -65,55 +46,7 @@ class MyApp extends StatelessWidget {
       ],
       locale: const Locale("fa","IR"),// initial default local
 
-      home: FutureBuilder(
-          future: jwtOrEmpty,
-          builder: (context, snapshot) {
-
-            if (!snapshot.hasData) return  LoginPage();
-            if (snapshot.data != "N") {
-              dynamic str = snapshot.data;
-              dynamic jwt = str.length > 1 ? str.toString().split(".") : "";
-              if (jwt.length != 3) {
-                return LoginPage();
-              } else {
-                var payload = json.decode(
-                    ascii.decode(base64.decode(base64.normalize(jwt[1]))));
-                if (DateTime.fromMillisecondsSinceEpoch(payload["exp"] * 1000)
-                    .isAfter(DateTime.now())) {
-                  return FutureBuilder(future: userRoleType,
-                      builder:(context, snapshot){
-                        print(snapshot.data);
-                        if (!snapshot.hasData) return  LoginPage();
-
-                        else{
-                          switch(snapshot.data){
-
-                            case "Admin":
-                              return Scaffold(body:AdminHomeScreen(str, payload));
-                              break;
-                            case "Designer":
-                              return Scaffold(body:DesignerHomeScreen(str, payload));
-                              break;
-                            case "Workshop1":
-                              return Scaffold(body:Workshop1HomeScreen(str, payload));
-                              break;
-                            case "Workshop2":
-                              return Scaffold(body:Workshop2HomeScreen(str, payload));
-                              break;
-
-                          }
-                          return  LoginPage();
-                        }
-                      } );
-
-                } else {
-                  return LoginPage();
-                }
-              }
-            } else {
-              return  LoginPage();
-            }
-          }),
+      home: HomeScreen(),
     );
   }
 }
