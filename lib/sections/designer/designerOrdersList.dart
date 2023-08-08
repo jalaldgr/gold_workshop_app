28f37c7f79efc01efdfe1c6f326d6ed6412f7ee9
Jalal Yadegar <jalaldgr@gmail.com>
2023-08-08 11:11:03 +0330

    add designer full name to orders list

--- lib/sections/designer/designerOrdersList.dart
+++ lib/sections/designer/designerOrdersList.dart
@@ -141,6 +141,7 @@ class _DesignerOrdersListState extends State<DesignerOrdersList> {
                       SizedBox(width: 40,),
                       Expanded(child: Column(crossAxisAlignment:CrossAxisAlignment.start,children: [Text("سفارش گیرنده",style: TextStyle(fontSize: 11),),Text("${order!.orderRecipient}")],)),
                       Expanded(child: Column(crossAxisAlignment:CrossAxisAlignment.start,children: [Text("وضعیت سفارش",style: TextStyle(fontSize: 11),),Text("${order!.status}")],)),
+                      Expanded(child: Column(crossAxisAlignment:CrossAxisAlignment.start,children: [Text("نام طراح",style: TextStyle(fontSize: 11),),Text("${order!.designerFullName}")],)),
                       Expanded(child:Row(children: [Text("${order.instantDelivery=="true" ? "✓ تحویل فوری":""}"),],)),
                       Expanded(child:Row(children: [Text("${order.paperDelivery=="true" ? "✓ کاغذی":""}"),],)),
                       Expanded(child:Row(children: [Text("${order.feeOrder=="true" ? "✓ بیعانه":""}"),],)),
--- lib/sections/workshop2/workshop2OrdersList.dart
+++ lib/sections/workshop2/workshop2OrdersList.dart
@@ -149,6 +149,7 @@ class _Workshop2OrdersListState extends State<Workshop2OrdersList> {
                       SizedBox(width: 40,),
                       Expanded(child: Column(crossAxisAlignment:CrossAxisAlignment.start,children: [Text("سفارش گیرنده",style: TextStyle(fontSize: 11),),Text("${order!.orderRecipient}")],)),
                       Expanded(child: Column(crossAxisAlignment:CrossAxisAlignment.start,children: [Text("وضعیت سفارش",style: TextStyle(fontSize: 11),),Text("${order!.status}")],)),
+                      Expanded(child: Column(crossAxisAlignment:CrossAxisAlignment.start,children: [Text("نام طراح",style: TextStyle(fontSize: 11),),Text("${order!.designerFullName}")],)),
                       Expanded(child:Row(children: [Text("${order.instantDelivery=="true" ? "✓ تحویل فوری":""}"),],)),
                       Expanded(child:Row(children: [Text("${order.paperDelivery=="true" ? "✓ کاغذی":""}"),],)),
                       Expanded(child:Row(children: [Text("${order.feeOrder=="true" ? "✓ بیعانه":""}"),],)),
